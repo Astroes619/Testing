44c18b33444cc13c16c8ac6b5548eaf14b5c1139
Atheeque Hasan <aiaahasan@students.nsbm.ac.lk>
2023-03-23 23:08:00 +0530

    test

---
+++ FinalModel/finaltest.py
@@ -0,0 +1,111 @@
+import cv2
+import csv
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+
+
+
+
+
+
+# initialize video capture
+cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+# create CSV file and write header
+with open('eye_tracking_data.csv', mode='w') as csv_file:
+    fieldnames = ['x', 'y', 'w', 'h', 'aspect_ratio', 'direction']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+
+# define function to get direction of gaze based on eye position
+def get_gaze_direction(eye_x, eye_y, eye_w, eye_h, face_w):
+    eye_center_x = eye_x + eye_w/2
+    eye_center_y = eye_y + eye_h/2
+    
+    if eye_center_x < face_w*0.4:
+        return 'left'
+    elif eye_center_x > face_w*0.6:
+        return 'right'
+    else:
+        return 'center'
+
+# Loop through the frames
+while True:
+    # Read the frame from the video capture object
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect the faces in the grayscale frame
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # Loop through each detected face
+    for (x, y, w, h) in faces:
+        # Draw a rectangle around the face
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # Crop the face region
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+        # Detect the eyes within the face region
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+
+        # Loop through each detected eye
+        for (ex, ey, ew, eh) in eyes:
+            # Draw a rectangle around the eye
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 0, 255), 2)
+
+            # Calculate aspect ratio of eye region
+            aspect_ratio = ew/eh
+
+            # Get the direction of gaze
+            gaze_direction = get_gaze_direction(ex, ey, ew, eh, w)
+
+            # write data to CSV file
+            with open('eye_tracking_data.csv', mode='a') as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                writer.writerow({'x': x, 'y': y, 'w': w, 'h': h, 'aspect_ratio': aspect_ratio, 'direction': gaze_direction})
+
+                # print data to terminal
+                print(f"x: {x}, y: {y}, w: {w}, h: {h}, aspect_ratio: {aspect_ratio}, direction: {gaze_direction}")
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
+    # Wait for a key press to exit
+    if cv2.waitKey(1)  == ord('q'):
+        break
+
+# Load the data from the CSV file
+data = []
+labels = []
+
+with open('eye_tracking_data.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        data.append([float(row['x']), float(row['y']), float(row['w']), float(row['h']), float(row['aspect_ratio'])])
+        labels.append(row['direction'])
+
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2)
+
+# Create a decision tree classifier and fit it to the training data
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+
+# Test the classifier on the testing data and print the accuracy
+accuracy = clf.score(X_test, y_test)
+print(f"Accuracy: {accuracy * 100:.2f}%")
+
+
+# Release video capture and close window
+cap.release()
+cv2.destroyAllWindows()
