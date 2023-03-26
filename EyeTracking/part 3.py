830a111d1ba4d2117fca18305e67dcd14f8f008a
Atheeque Hasan <aiaahasan@students.nsbm.ac.lk>
2023-03-26 10:32:49 +0530

    use of svc

--- EyeTracking/part 3.py
+++ EyeTracking/part 3.py
@@ -1,97 +1,98 @@
 import cv2
 import csv
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
-# initialize video capture
+# Initialize video capture
 cap = cv2.VideoCapture(0)
 
-# load classifiers
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-# Load phone cascade 
-phone_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_phone.xml')
 
-# create CSV file and write header
-with open('eye_tracking_data.csv', mode='w') as csv_file:
-    fieldnames = ['x', 'y', 'w', 'h', 'aspect_ratio', 'direction', 'phone_detected']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
+# Initialize an empty list to store data
+eye_tracking_data = []
 
-# define function to get direction of gaze based on eye position
 def get_gaze_direction(eye_x, eye_y, eye_w, eye_h, face_w):
     eye_center_x = eye_x + eye_w/2
     eye_center_y = eye_y + eye_h/2
     
-    if eye_center_x < face_w*0.4:
-        return 'left'
-    elif eye_center_x > face_w*0.6:
-        return 'right'
-    else:
-        return 'center'
+    left_range = face_w * 0.35
+    right_range = face_w * 0.65
+    center_range_left = face_w * 0.325
+    center_range_right = face_w * 0.675
 
-# define function to detect phone in image
-def detect_phone(frame, phone_cascade):
-    phones = phone_cascade.detectMultiScale(frame, 1.3, 5)
-    if len(phones) > 0:
-        return True
+    if eye_center_x < left_range:
+        return 'left'
+    elif eye_center_x > right_range:
+        return 'right'
+    elif center_range_left < eye_center_x < center_range_right:
+        return 'center'
     else:
-        return False
+        return 'unknown'
 
 # Loop through the frames
 while True:
-    # Read the frame from the video capture object
     ret, frame = cap.read()
 
-    # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect the faces in the grayscale frame
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # Loop through each detected face
     for (x, y, w, h) in faces:
-        # Draw a rectangle around the face
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-        # Crop the face region
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
 
-        # Detect the eyes within the face region
         eyes = eye_cascade.detectMultiScale(roi_gray)
 
-        # Loop through each detected eye
         for (ex, ey, ew, eh) in eyes:
-            # Draw a rectangle around the eye
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 0, 255), 2)
 
-            # Calculate aspect ratio of eye region
+            eye_img = roi_gray[ey:ey+eh, ex:ex+ew]
+            eye_img = cv2.resize(eye_img, (100, 50))
+
             aspect_ratio = ew/eh
 
-            # Get the direction of gaze
             gaze_direction = get_gaze_direction(ex, ey, ew, eh, w)
 
-            # Detect phone in image
-            phone_detected = detect_phone(roi_gray, phone_cascade)
+            # Append data to the list instead of writing to the CSV file
+            eye_tracking_data.append({'x': x, 'y': y, 'w': w, 'h': h, 'aspect_ratio': aspect_ratio, 'direction': gaze_direction})
 
-            # write data to CSV file
-            with open('eye_tracking_data.csv', mode='a') as csv_file:
-                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-                writer.writerow({'x': x, 'y': y, 'w': w, 'h': h, 'aspect_ratio': aspect_ratio, 'direction': gaze_direction, 'phone_detected': phone_detected})
+            print(f"x: {x}, y: {y}, w: {w}, h: {h}, aspect_ratio: {aspect_ratio}, direction: {gaze_direction}")
 
-            # print data to terminal
-            print(f"x: {x}, y: {y}, w: {w}, h: {h}, aspect_ratio: {aspect_ratio}, direction: {gaze_direction}, phone_detected: {phone_detected}")
-
-            # If phone is detected, draw a rectangle around it
-            if phone_detected:
-                phones = phone_cascade.detectMultiScale(roi_gray)
-                for (px, py, pw, ph) in phones:
-                    cv2.rectangle(roi_color, (px, py), (px+pw, py+ph), (255, 0, 0), 2)
-
-    # Display the resulting frame
     cv2.imshow('frame', frame)
-   
-    if cv2.waitKey(1) == ord('q'):
+
+    if cv2.waitKey(1)  == ord('q'):
         break
+
+# Write the data to the CSV file
+with open('eye_tracking_data.csv', mode='w') as csv_file:
+    fieldnames = ['x', 'y', 'w', 'h', 'aspect_ratio', 'direction']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(eye_tracking_data)
+
+# Load the data from the list
+data = []
+labels = []
+
+for row in eye_tracking_data:
+    data.append([float(row['x']), float(row['y']), float(row['w']), float(row['h']), float(row['aspect_ratio'])])
+    labels.append(row['direction'])
+
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2)
+
+# Create an SVM classifier and fit it to the training data
+clf = SVC(kernel='linear', C=1)
+clf.fit(X_train, y_train)
+
+# Test the classifier on the testing data and print the accuracy
+accuracy = clf.score(X_test, y_test)
+print(f"Accuracy: {accuracy * 100:.2f}%")
 
 # Release video capture and close window
 cap.release()
