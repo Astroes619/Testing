1fa409b9e1ea436c487548e2346a19259a698c64
Atheeque Hasan <aiaahasan@students.nsbm.ac.lk>
2023-03-29 14:12:23 +0530

    image

--- EyeTracking/test 4.py
+++ EyeTracking/test 4.py
@@ -1,80 +1,82 @@
 import cv2
+import os
 import csv
 
-# initialize video capture
+# Initialize video capture
 cap = cv2.VideoCapture(0)
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-# create CSV file and write header
-with open('eye_tracking_data.csv', mode='w') as csv_file:
-    fieldnames = ['x', 'y', 'w', 'h', 'direction']
+# Create a directory to save eye images
+if not os.path.exists('eye_images'):
+    os.makedirs('eye_images')
+
+# Create directories for left, center, and right gazes
+directions = ['left', 'center', 'right']
+for direction in directions:
+    if not os.path.exists(f'eye_images/{direction}'):
+        os.makedirs(f'eye_images/{direction}')
+
+# Create a CSV file to store the image paths and labels
+with open('eye_tracking_data.csv', mode='a') as csv_file:
+    fieldnames = ['image_path', 'label']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
 
-# define gaze direction thresholds as constants
-GAZE_LEFT_THRESHOLD = 0.4
-GAZE_RIGHT_THRESHOLD = 0.6
+img_count = 0
 
-# define function to get direction of gaze based on eye position
-def get_gaze_direction(eye_x, eye_y, eye_w, eye_h, face_w):
-    eye_center_x = eye_x + eye_w/2
-    eye_center_y = eye_y + eye_h/2
-    
-    if eye_center_x < face_w * GAZE_LEFT_THRESHOLD:
-        return 'left'
-    elif eye_center_x > face_w * GAZE_RIGHT_THRESHOLD:
-        return 'right'
-    else:
-        return 'center'
-
-# Loop through the frames
 while True:
-    # Read the frame from the video capture object
     ret, frame = cap.read()
-
-    # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Detect the faces in the grayscale frame
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # Loop through each detected face
     for (x, y, w, h) in faces:
-        # Draw a rectangle around the face
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-        # Crop the face region
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
 
-        # Detect the eyes within the face region
         eyes = eye_cascade.detectMultiScale(roi_gray)
 
-        # Loop through each detected eye
         for (ex, ey, ew, eh) in eyes:
-            # Draw a rectangle around the eye
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 0, 255), 2)
 
-            # Get the direction of gaze
-            gaze_direction = get_gaze_direction(ex, ey, ew, eh, w)
+            # Save the preprocessed eye image
+            eye_img = roi_gray[ey:ey+eh, ex:ex+ew]
+            eye_img = cv2.resize(eye_img, (100, 50))
 
-            # write data to CSV file
+            cv2.imshow('eye', eye_img)
+
+            # Manually label the data as 'left', 'center', or 'right'
+            key = cv2.waitKey(0) & 0xFF
+
+            if key == ord('q'):
+                break
+
+            if key == ord('l'):  # Left
+                label = 'left'
+            elif key == ord('c'):  # Center
+                label = 'center'
+            elif key == ord('r'):  # Right
+                label = 'right'
+            else:
+                continue
+
+            img_path = f'eye_images/{label}/eye_{img_count}.png'
+            cv2.imwrite(img_path, eye_img)
+            img_count += 1
+
+            # Write the image path and label to the CSV file
             with open('eye_tracking_data.csv', mode='a') as csv_file:
                 writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-                writer.writerow({'x': x, 'y': y, 'w': w, 'h': h, 'direction': gaze_direction})
+                writer.writerow({'image_path': img_path, 'label': label})
 
-                # print data to terminal
-                print(f"x: {x}, y: {y}, w: {w}, h: {h}, direction: {gaze_direction}")
-
-    # Display the resulting frame
     cv2.imshow('frame', frame)
 
-    # Wait for a key press to exit
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(100) == ord('q'):
         break
 
-# release video capture and close window
+# Release video capture and close window
 cap.release()
 cv2.destroyAllWindows()
